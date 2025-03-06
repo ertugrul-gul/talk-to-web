23f18ebea419c70d192db3e001483b749e51d32c
ertugul0848 <114884594+ertugul0848@users.noreply.github.com>
2025-03-06 16:10:23 +0300

    few

---
+++ deneme.py
@@ -0,0 +1,11 @@
+"""
+
+list1=("ırmak","gizem", "ece")
+list2=("python", "c", "c++")
+
+list3=list1+list2
+print(type(list3))
+
+list3 to dict
+
+"""
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+'''
+
 print("X degerini giriniz")
 x = input()
 print("Y degerini giriniz")
@@ -6,3 +8,11 @@ y = input()
 toplam = int(x) + int(y)
 
 print("{0} {1} {2}".format("Toplam ","=",toplam))
+'''
+
+str = 'verl fdsf'
+
+print(str [::-1])
+
+for item in str:
+    print(item)
